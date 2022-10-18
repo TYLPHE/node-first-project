757aafba032ce172f30563f3671da41187d84283
TYLPHE <tylphet@gmail.com>
2022-10-18 14:08:21 -0600

    finished assignment and ready to post

---
+++ index.mjs
@@ -0,0 +1,33 @@
+import * as http from 'node:http';
+import * as fs from 'node:fs';
+
+const hostName = 'localhost';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  const myURL = new URL(req.url, `http://${hostName}:${port}/`);  
+  let fileName = '';
+  (myURL.pathname === '/') ? fileName = `index` : fileName = myURL.pathname;
+
+  fs.readFile(`./${fileName}.html`, (err, data) => {
+
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write(
+        fs.readFileSync('./404.html', (err, data) => {
+          if (err) throw (err);
+          return data;
+        })
+      );
+      return res.end();
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    }
+  });
+})
+
+server.listen(port, hostName, () => {
+  console.log(`Server running at http://${hostName}:${port}/`)
+})
